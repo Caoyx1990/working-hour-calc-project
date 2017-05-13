b0d8cf43395cbf381dbdd1b791012ccc6ad710ce
caoyx <caoyx1990@outlook.com>
2017-05-13 19:00:52 +0800

    caoyx

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "index";
     }
 
     public static void main(String[] args) throws Exception {
